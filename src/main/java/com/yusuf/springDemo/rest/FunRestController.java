c1906eebaa332d07fc648a4f6fe694986b4302b0
Yusuf <yusufsonmez951@gmail.com>
2023-07-27 22:31:34 +0300

    application.properties demo

--- src/main/java/com/yusuf/springDemo/rest/FunRestController.java
+++ src/main/java/com/yusuf/springDemo/rest/FunRestController.java
@@ -1,11 +1,16 @@
 package com.yusuf.springDemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
     // expose "/" that return "Hello World"
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
 
     @GetMapping("/")
     public String sayHello() {
@@ -24,5 +29,10 @@ public class FunRestController {
     @GetMapping("/fortune")
     public  String getDailyFortune() {
         return "Today is your lucky day";
+    }
+
+    @GetMapping("/team-info")
+    public String getTeamName() {
+        return "Coach : " + coachName + " ,\nTeam name: " + teamName;
     }
 }
