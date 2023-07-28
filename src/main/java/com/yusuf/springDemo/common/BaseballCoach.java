71a9d82edb916bd69d1cddec08353237522a9b43
Yusuf <yusufsonmez951@gmail.com>
2023-07-29 01:27:54 +0300

    Lazy-initialization demo

--- src/main/java/com/yusuf/springDemo/common/BaseballCoach.java
+++ src/main/java/com/yusuf/springDemo/common/BaseballCoach.java
@@ -1,16 +1,14 @@
 package com.yusuf.springDemo.common;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TennisCoach implements Coach{
-
-    public TennisCoach() {
+public class BaseballCoach implements Coach{
+    public BaseballCoach() {
         System.out.println("In constructor : " + getClass().getName());
     }
     @Override
     public String getDailyWorkout() {
-        return "do soma stuff";
+        return "do more stuff";
     }
 }
--- src/main/java/com/yusuf/springDemo/common/CricketCoach.java
+++ src/main/java/com/yusuf/springDemo/common/CricketCoach.java
@@ -1,11 +1,16 @@
 package com.yusuf.springDemo.common;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CricketCoach implements Coach {
 
+    public CricketCoach() {
+        System.out.println("In constructor : " + getClass().getName());
+    }
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes";
